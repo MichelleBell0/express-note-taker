b4a80ed012a700f15b77b4433126f9d9ac737457
Michelle Bell <michbesch0@outlook.com>
2024-05-16 15:38:15 -0400

    Added api routes in routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,17 +8,18 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
-
-// GET route for the landing page
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET route for the notes page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET route for the landing page
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => 
